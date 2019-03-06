ffbe9bd669f012718594c1dadbf867c8afe5c486
Chris Tate <chris@austincodeshop.com>
2019-03-05 18:09:10 -0600

    adds missing files

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,5 +1,7 @@
 export * from './Gateway';
+export * from './GatewayError';
 export * from './GatewayMethod';
 export * from './GatewayRoute';
 export * from './Service';
 export * from './ServiceCall';
+export * from './ServiceError';
