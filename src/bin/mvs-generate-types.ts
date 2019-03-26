7eaa66cdbd6a68c7491014798cead572a792458b
Chris Tate <chris@austincodeshop.com>
2019-03-26 01:34:14 -0500

    splits mvs scripts

--- src/bin/mvs-generate-types.ts
+++ src/bin/mvs-generate-types.ts
@@ -11,51 +11,13 @@ interface IGenerateOpts {
   type: string;
 }
 
-export function generateHandler(opts: IGenerateOpts) {
+function generateHandler(opts: IGenerateOpts) {
   const files = opts.type === 'gateway' ? {
     autoBarrel: {
       filename: `${opts.path}/.auto/index.ts`,
       engine: 'handlebars',
       template: 'gatewayAutoBarrel'
     },
-    tsClient: {
-      filename: `${opts.path}/.auto/clients/src/${Case.pascal(opts.name)}GatewayClient.ts`,
-      engine: 'handlebars',
-      template: 'gatewayTsClient',
-      mapping: {
-        gatewayName: opts.name,
-        routes: '/interfaces'
-      }
-    },
-    clientIndex: {
-      filename: `${opts.path}/.auto/clients/src/index.ts`,
-      engine: 'handlebars',
-      template: 'gatewayClientIndex',
-      mapping: {
-        name: opts.name
-      }
-    },
-    clientPackage: {
-      filename: `${opts.path}/.auto/clients/package.json`,
-      engine: 'handlebars',
-      template: 'gatewayClientPackage',
-      mapping: {
-        packageName: opts.packageName
-      }
-    },
-    clientTsConfig: {
-      filename: `${opts.path}/.auto/clients/tsconfig.json`,
-      engine: 'handlebars',
-      template: 'gatewayClientTsConfig'
-    },
-    // swiftClient: {
-    //   filename: `${opts.path}/.auto/clients/${Case.pascal(opts.name)}GatewayClient.swift`,
-    //   engine: 'handlebars',
-    //   template: 'gatewaySwiftClient',
-    //   mapping: {
-    //     routes: '/interfaces'
-    //   }
-    // },
     routeInterface: {
       filename: {
         engine: 'handlebars',
@@ -91,44 +53,6 @@ export function generateHandler(opts: IGenerateOpts) {
       engine: 'handlebars',
       template: 'autoBarrel'
     },
-    tsClient: {
-      filename: `${opts.path}/.auto/clients/src/${Case.pascal(opts.name)}ServiceClient.ts`,
-      engine: 'handlebars',
-      template: 'tsClient',
-      mapping: {
-        serviceName: opts.name,
-        calls: '/interfaces'
-      }
-    },
-    clientIndex: {
-      filename: `${opts.path}/.auto/clients/src/index.ts`,
-      engine: 'handlebars',
-      template: 'serviceClientIndex',
-      mapping: {
-        name: opts.name
-      }
-    },
-    clientPackage: {
-      filename: `${opts.path}/.auto/clients/package.json`,
-      engine: 'handlebars',
-      template: 'serviceClientPackage',
-      mapping: {
-        packageName: opts.packageName
-      }
-    },
-    clientTsConfig: {
-      filename: `${opts.path}/.auto/clients/tsconfig.json`,
-      engine: 'handlebars',
-      template: 'serviceClientTsConfig'
-    },
-    // swiftClient: {
-    //   filename: `${opts.path}/.auto/clients/${Case.pascal(opts.name)}ServiceClient.swift`,
-    //   engine: 'handlebars',
-    //   template: 'swiftClient',
-    //   mapping: {
-    //     calls: '/interfaces'
-    //   }
-    // },
     callInterface: {
       filename: {
         engine: 'handlebars',
@@ -209,21 +133,6 @@ export function generateHandler(opts: IGenerateOpts) {
       interfacesBarrel: {
         template: fs.readFileSync(`${__dirname}/../../bin/templates/service/interfacesBarrel.hbs`, 'utf8')
       },
-      // swiftClient: {
-      //   template: fs.readFileSync(`${__dirname}/../../bin/templates/service/swiftClient.hbs`, 'utf8')
-      // },
-      tsClient: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/service/tsClient.hbs`, 'utf8')
-      },
-      serviceClientIndex: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/service/clientIndex.hbs`, 'utf8')
-      },
-      serviceClientPackage: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/service/clientPackage.hbs`, 'utf8')
-      },
-      serviceClientTsConfig: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/service/clientTsConfig.hbs`, 'utf8')
-      },
       gatewayRouteInterface: {
         template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/routeInterface.hbs`, 'utf8')
       },
@@ -235,21 +144,6 @@ export function generateHandler(opts: IGenerateOpts) {
       },
       gatewayInterfacesBarrel: {
         template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/interfacesBarrel.hbs`, 'utf8')
-      },
-      // gatewaySwiftClient: {
-      //   template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/swiftClient.hbs`, 'utf8')
-      // },
-      gatewayTsClient: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/tsClient.hbs`, 'utf8')
-      },
-      gatewayClientIndex: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/clientIndex.hbs`, 'utf8')
-      },
-      gatewayClientPackage: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/clientPackage.hbs`, 'utf8')
-      },
-      gatewayClientTsConfig: {
-        template: fs.readFileSync(`${__dirname}/../../bin/templates/gateway/clientTsConfig.hbs`, 'utf8')
       }
     }
   });
@@ -265,123 +159,27 @@ export function generateHandler(opts: IGenerateOpts) {
   // console.log(JSON.stringify(items, null, 2));
 }
 
-const isGateway = fs.existsSync('./src/routes');
-const isService = fs.existsSync('./src/calls');
+export function generateTypes() {
+  const isGateway = fs.existsSync('./src/routes');
+  const isService = fs.existsSync('./src/calls');
 
-if (isGateway) {
-  const interfaces: any = {};
-  const gatewayRoutes = fs.readdirSync(`./src/routes`)
-    .filter(gatewayRoute => !/\.ts$/i.test(gatewayRoute));
-  gatewayRoutes.forEach((gatewayRoute) => {
-    const routeObject = require(path.normalize(`${process.cwd()}/src/routes/${gatewayRoute}`));
-    let uri = '';
-    Object.keys(routeObject).forEach((key) => {
-      if (routeObject[key] instanceof Function) {
-        const route = new routeObject[key]();
-        if (route.uri) {
-          uri = route.uri;
-        }
-      }
-    });
-    const methods = fs.readdirSync(`./src/routes/${gatewayRoute}`)
-      .filter(method => !/\.ts$/i.test(method));
-    methods.forEach((method) => {
-      const schemaItem = fs.readdirSync(`./src/routes/${gatewayRoute}/${method}`)
-        .filter(item => /\.schema\.ts$/i.test(item))[0];
-
-      const interfaceObject = require(path.normalize(`${process.cwd()}/src/routes/${gatewayRoute}/${method}/${schemaItem}`));
-      Object.keys(interfaceObject).forEach((key) => {
-        const name = Case.camel(key).replace(/Schema$/, '');
-        if (!interfaces[gatewayRoute]) {
-          interfaces[gatewayRoute] = {
-            methods: {},
-            uri
-          };
-        }
-        interfaces[gatewayRoute].methods[method] = {
-          ...interfaceObject[key],
-          name
-        };
-      });
-    });
-  });
-
-  const pkg = require(path.normalize(`${process.cwd()}/package.json`));
-  const gatewayName = Case.pascal(pkg.name.split('gateway-')[1]);
-
-  generateHandler({
-    packageName: pkg.name,
-    name: gatewayName,
-    type: 'gateway',
-    path: `./src`,
-    specs: {
-      interfaces
-    }
-  });
-}
-
-if (isService) {
-  const interfaces: any = {};
-  const items = fs.readdirSync(`./src/calls`)
-    .filter(item => !/\.ts$/i.test(item));
-  items.forEach((item) => {
-    const schemaItem = fs.readdirSync(`./src/calls/${item}`)
-      .filter(item => /\.schema\.ts$/i.test(item))[0];
-
-    const interfaceObject = require(path.normalize(`${process.cwd()}/src/calls/${item}/${schemaItem}`));
-    Object.keys(interfaceObject).forEach((key) => {
-      const name = Case.camel(key).replace(/Schema$/, '');
-      interfaces[name] = interfaceObject[key];
-    });
-  });
-
-  const pkg = require(path.normalize(`${process.cwd()}/package.json`));
-  const serviceName = Case.pascal(pkg.name.split('service-')[1]);
-
-  generateHandler({
-    packageName: pkg.name,
-    name: serviceName,
-    type: 'service',
-    path: `./src`,
-    specs: {
-      interfaces
-    }
-  });
-}
-
-if (!isGateway && !isService) {
-  const pkg = require(path.normalize(`${process.cwd()}/package.json`));
-  
-  const gateways = fs.readdirSync('./src/gateways')
-    .filter(gateway => fs.lstatSync(`./src/gateways/${gateway}`).isDirectory());
-  gateways.forEach((gateway) => {
+  if (isGateway) {
     const interfaces: any = {};
-    const gatewayRoutes = fs.readdirSync(`./src/gateways/${gateway}/routes`)
+    const gatewayRoutes = fs.readdirSync(`./src/routes`)
       .filter(gatewayRoute => !/\.ts$/i.test(gatewayRoute));
     gatewayRoutes.forEach((gatewayRoute) => {
-      const routeObject = require(path.normalize(`${process.cwd()}/src/gateways/${gateway}/routes/${gatewayRoute}`));
-      let uri = '';
-      Object.keys(routeObject).forEach((key) => {
-        if (routeObject[key] instanceof Function) {
-          const route = new routeObject[key]();
-          if (route.uri) {
-            uri = route.uri;
-          }
-        }
-      });
-      const methods = fs.readdirSync(`./src/gateways/${gateway}/routes/${gatewayRoute}`)
+      const methods = fs.readdirSync(`./src/routes/${gatewayRoute}`)
         .filter(method => !/\.ts$/i.test(method));
       methods.forEach((method) => {
-        const schemaItem = fs.readdirSync(`./src/gateways/${gateway}/routes/${gatewayRoute}/${method}`)
+        const schemaItem = fs.readdirSync(`./src/routes/${gatewayRoute}/${method}`)
           .filter(item => /\.schema\.ts$/i.test(item))[0];
 
-        const interfaceObject = require(path.normalize(`${process.cwd()}/src/gateways/${gateway}/routes/${gatewayRoute}/${method}/${schemaItem}`));
+        const interfaceObject = require(path.normalize(`${process.cwd()}/src/routes/${gatewayRoute}/${method}/${schemaItem}`));
         Object.keys(interfaceObject).forEach((key) => {
           const name = Case.camel(key).replace(/Schema$/, '');
           if (!interfaces[gatewayRoute]) {
             interfaces[gatewayRoute] = {
-              methods: {},
-              uri
+              methods: {}
             };
           }
           interfaces[gatewayRoute].methods[method] = {
@@ -392,42 +190,129 @@ if (!isGateway && !isService) {
       });
     });
 
+    const pkg = require(path.normalize(`${process.cwd()}/package.json`));
+    const gatewayName = Case.pascal(pkg.name.split('gateway-')[1]);
+
     generateHandler({
       packageName: pkg.name,
-      name: gateway,
+      name: gatewayName,
       type: 'gateway',
-      path: `./src/gateways/${gateway}`,
+      path: `./src`,
       specs: {
         interfaces
       }
     });
-  });
+  }
 
-  const services = fs.readdirSync('./src/services')
-    .filter(service => fs.lstatSync(`./src/services/${service}`).isDirectory())
-  services.forEach((service) => {
+  if (isService) {
     const interfaces: any = {};
-    const items = fs.readdirSync(`./src/services/${service}/calls`)
+    const items = fs.readdirSync(`./src/calls`)
       .filter(item => !/\.ts$/i.test(item));
     items.forEach((item) => {
-      const schemaItem = fs.readdirSync(`./src/services/${service}/calls/${item}`)
+      const schemaItem = fs.readdirSync(`./src/calls/${item}`)
         .filter(item => /\.schema\.ts$/i.test(item))[0];
 
-      const interfaceObject = require(path.normalize(`${process.cwd()}/src/services/${service}/calls/${item}/${schemaItem}`));
+      const interfaceObject = require(path.normalize(`${process.cwd()}/src/calls/${item}/${schemaItem}`));
       Object.keys(interfaceObject).forEach((key) => {
         const name = Case.camel(key).replace(/Schema$/, '');
         interfaces[name] = interfaceObject[key];
       });
     });
 
+    const pkg = require(path.normalize(`${process.cwd()}/package.json`));
+    const serviceName = Case.pascal(pkg.name.split('service-')[1]);
+
     generateHandler({
       packageName: pkg.name,
-      name: service,
+      name: serviceName,
       type: 'service',
-      path: `./src/services/${service}`,
+      path: `./src`,
       specs: {
         interfaces
       }
     });
-  });
+  }
+
+  if (!isGateway && !isService) {
+    const pkg = require(path.normalize(`${process.cwd()}/package.json`));
+    
+    const gateways = fs.readdirSync('./src/gateways')
+      .filter(gateway => fs.lstatSync(`./src/gateways/${gateway}`).isDirectory());
+    gateways.forEach((gateway) => {
+      const interfaces: any = {};
+      const gatewayRoutes = fs.readdirSync(`./src/gateways/${gateway}/routes`)
+        .filter(gatewayRoute => !/\.ts$/i.test(gatewayRoute));
+      gatewayRoutes.forEach((gatewayRoute) => {
+        const routeObject = require(path.normalize(`${process.cwd()}/src/gateways/${gateway}/routes/${gatewayRoute}`));
+        let uri = '';
+        Object.keys(routeObject).forEach((key) => {
+          if (routeObject[key] instanceof Function) {
+            const route = new routeObject[key]();
+            if (route.uri) {
+              uri = route.uri;
+            }
+          }
+        });
+        const methods = fs.readdirSync(`./src/gateways/${gateway}/routes/${gatewayRoute}`)
+          .filter(method => !/\.ts$/i.test(method));
+        methods.forEach((method) => {
+          const schemaItem = fs.readdirSync(`./src/gateways/${gateway}/routes/${gatewayRoute}/${method}`)
+            .filter(item => /\.schema\.ts$/i.test(item))[0];
+
+          const interfaceObject = require(path.normalize(`${process.cwd()}/src/gateways/${gateway}/routes/${gatewayRoute}/${method}/${schemaItem}`));
+          Object.keys(interfaceObject).forEach((key) => {
+            const name = Case.camel(key).replace(/Schema$/, '');
+            if (!interfaces[gatewayRoute]) {
+              interfaces[gatewayRoute] = {
+                methods: {},
+                uri
+              };
+            }
+            interfaces[gatewayRoute].methods[method] = {
+              ...interfaceObject[key],
+              name
+            };
+          });
+        });
+      });
+
+      generateHandler({
+        packageName: pkg.name,
+        name: gateway,
+        type: 'gateway',
+        path: `./src/gateways/${gateway}`,
+        specs: {
+          interfaces
+        }
+      });
+    });
+
+    const services = fs.readdirSync('./src/services')
+      .filter(service => fs.lstatSync(`./src/services/${service}`).isDirectory())
+    services.forEach((service) => {
+      const interfaces: any = {};
+      const items = fs.readdirSync(`./src/services/${service}/calls`)
+        .filter(item => !/\.ts$/i.test(item));
+      items.forEach((item) => {
+        const schemaItem = fs.readdirSync(`./src/services/${service}/calls/${item}`)
+          .filter(item => /\.schema\.ts$/i.test(item))[0];
+
+        const interfaceObject = require(path.normalize(`${process.cwd()}/src/services/${service}/calls/${item}/${schemaItem}`));
+        Object.keys(interfaceObject).forEach((key) => {
+          const name = Case.camel(key).replace(/Schema$/, '');
+          interfaces[name] = interfaceObject[key];
+        });
+      });
+
+      generateHandler({
+        packageName: pkg.name,
+        name: service,
+        type: 'service',
+        path: `./src/services/${service}`,
+        specs: {
+          interfaces
+        }
+      });
+    });
+  }
 }
--- src/bin/mvs.ts
+++ src/bin/mvs.ts
@@ -1,8 +1,22 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import { generateClient } from './mvs-generate-client';
 import { generateTypes } from './mvs-generate-types';
 
-if (process.argv[2] === 'client') {
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf-8'));
+
+console.log(`mvs v${pkg.version}`);
+
+if (process.argv[2] !== 'generate' || ['client', 'types'].indexOf(process.argv[3]) === -1) {
+  console.log('Usage: mvs generate <client|types>');
+  process.exit(0);
+}
+
+if (process.argv[3] === 'client') {
+  console.log('Generating client...');
   generateClient();
-} else {
+} else if (process.argv[3] === 'types') {
+  console.log('Generating types...');
   generateTypes();
 }
