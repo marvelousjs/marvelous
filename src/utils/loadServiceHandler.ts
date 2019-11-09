8dd59a201335f9bd85c6a1b753d52f9c3ad489d3
Chris Tate <chris@austincodeshop.com>
2019-11-08 18:00:41 -0600

    handles ServiceError

--- src/utils/loadServiceHandler.ts
+++ src/utils/loadServiceHandler.ts
@@ -1,6 +1,7 @@
 import * as validator from 'is-my-json-valid';
 
 import { ValidationServiceError } from '../errors';
+import { ServiceError } from '../classes';
 
 export interface ILoadServiceHandlerOpts {
   enableLogging?: boolean;
@@ -12,41 +13,45 @@ export function loadServiceHandler(callClass: any, opts: ILoadServiceHandlerOpts
     : callClass;
 
   return async (request: any = {}) => {
-    if (opts.enableLogging) {
-      console.log('REQUEST - ', request);
-    }
-
-    // validate request
-    if (call.schema && call.schema.request) {
-      const validateRequest = validator(call.schema.request as any);
-      const requestIsValid = validateRequest(request);
-      if (!requestIsValid) {
-        const errorMessage = `"${validateRequest.errors[0].field.replace(/^data\./, '')}" ${
-          validateRequest.errors[0].message
-        }`;
-        throw new ValidationServiceError(`Invalid Request: ${errorMessage}`);
+    try {
+      if (opts.enableLogging) {
+        console.log('REQUEST - ', request);
       }
-    }
 
-    // get reponse
-    const response = await call.handler(request);
-
-    if (opts.enableLogging) {
-      console.log('RESPONSE - ', response);
-    }
-
-    // validate response
-    if (call.schema && call.schema.response) {
-      const validateResponse = validator(call.schema.response as any);
-      const responseIsValid = validateResponse(response);
-      if (!responseIsValid) {
-        const errorMessage = `"${validateResponse.errors[0].field.replace(/^data\./, '')}" ${
-          validateResponse.errors[0].message
-        }`;
-        throw new ValidationServiceError(`Invalid Response: ${errorMessage}`);
+      // validate request
+      if (call.schema && call.schema.request) {
+        const validateRequest = validator(call.schema.request as any);
+        const requestIsValid = validateRequest(request);
+        if (!requestIsValid) {
+          const errorMessage = `"${validateRequest.errors[0].field.replace(/^data\./, '')}" ${
+            validateRequest.errors[0].message
+          }`;
+          throw new ValidationServiceError(`Invalid Request: ${errorMessage}`);
+        }
       }
-    }
 
-    return response;
+      // get reponse
+      const response = await call.handler(request);
+
+      if (opts.enableLogging) {
+        console.log('RESPONSE - ', response);
+      }
+
+      // validate response
+      if (call.schema && call.schema.response) {
+        const validateResponse = validator(call.schema.response as any);
+        const responseIsValid = validateResponse(response);
+        if (!responseIsValid) {
+          const errorMessage = `"${validateResponse.errors[0].field.replace(/^data\./, '')}" ${
+            validateResponse.errors[0].message
+          }`;
+          throw new ValidationServiceError(`Invalid Response: ${errorMessage}`);
+        }
+      }
+
+      return response;
+    } catch (error) {
+      throw new ServiceError(error.message);
+    }
   };
 }
