d059254d721163af514a3056a4465e0bee958178
Chris Tate <chris@austincodeshop.com>
2020-08-03 08:57:53 -0500

    fixes removeEmpty

---
+++ src/tests/removeEmpty.spec.ts
@@ -0,0 +1,329 @@
+import * as assert from 'assert';
+
+import { removeEmpty } from '../utils';
+
+describe('removeEmpty', () => {
+  it('should remove undefined properties from object', () => {
+    const oldObject: {
+      firstName: string;
+      lastName: string;
+    } = {
+      firstName: 'Test',
+      lastName: undefined
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      firstName: 'Test',
+      lastName: undefined
+    });
+
+    assert.deepStrictEqual(newObject, {
+      firstName: 'Test'
+    });
+  });
+
+  it('should remove null properties from object', () => {
+    const oldObject: {
+      firstName: string;
+      lastName: string;
+    } = {
+      firstName: 'Test',
+      lastName: null
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      firstName: 'Test',
+      lastName: null
+    });
+
+    assert.deepStrictEqual(newObject, {
+      firstName: 'Test'
+    });
+  });
+
+  it('should remove undefined properties from nested object', () => {
+    const oldObject: {
+      person: {
+        firstName: string;
+        lastName: string;
+      };
+    } = {
+      person: {
+        firstName: 'Test',
+        lastName: undefined
+      }
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      person: {
+        firstName: 'Test',
+        lastName: undefined
+      }
+    });
+
+    assert.deepStrictEqual(newObject, {
+      person: {
+        firstName: 'Test'
+      }
+    });
+  });
+
+  it('should remove null properties from nested object', () => {
+    const oldObject: {
+      person: {
+        firstName: string;
+        lastName: string;
+      };
+    } = {
+      person: {
+        firstName: 'Test',
+        lastName: null
+      }
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      person: {
+        firstName: 'Test',
+        lastName: null
+      }
+    });
+
+    assert.deepStrictEqual(newObject, {
+      person: {
+        firstName: 'Test'
+      }
+    });
+  });
+
+  it('should remove undefined properties from arrays', () => {
+    const oldObject: {
+      people: string[];
+    } = {
+      people: ['Test', undefined]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: ['Test', undefined]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: ['Test']
+    });
+  });
+
+  it('should remove null properties from arrays', () => {
+    const oldObject: {
+      people: string[];
+    } = {
+      people: ['Test', null]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: ['Test', null]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: ['Test']
+    });
+  });
+
+  it('should remove undefined properties from array of objects', () => {
+    const oldObject: {
+      people: {
+        firstName: string;
+        lastName: string;
+      }[];
+    } = {
+      people: [
+        {
+          firstName: 'Test',
+          lastName: undefined
+        }
+      ]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: [
+        {
+          firstName: 'Test',
+          lastName: undefined
+        }
+      ]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: [
+        {
+          firstName: 'Test'
+        }
+      ]
+    });
+  });
+
+  it('should remove null properties from array of objects', () => {
+    const oldObject: {
+      people: {
+        firstName: string;
+        lastName: string;
+      }[];
+    } = {
+      people: [
+        {
+          firstName: 'Test',
+          lastName: null
+        }
+      ]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: [
+        {
+          firstName: 'Test',
+          lastName: null
+        }
+      ]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: [
+        {
+          firstName: 'Test'
+        }
+      ]
+    });
+  });
+
+  it('should remove undefined properties from array of arrays', () => {
+    const oldObject: {
+      people: string[][];
+    } = {
+      people: [['Test', undefined]]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: [['Test', undefined]]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: [['Test']]
+    });
+  });
+
+  it('should remove null properties from array of arrays', () => {
+    const oldObject: {
+      people: string[][];
+    } = {
+      people: [['Test', null]]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: [['Test', null]]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: [['Test']]
+    });
+  });
+
+  it('should remove undefined properties from array of array of objects', () => {
+    const oldObject: {
+      people: {
+        firstName: string;
+        lastName: string;
+      }[][];
+    } = {
+      people: [
+        [
+          {
+            firstName: 'Test',
+            lastName: undefined
+          }
+        ]
+      ]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: [
+        [
+          {
+            firstName: 'Test',
+            lastName: undefined
+          }
+        ]
+      ]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: [
+        [
+          {
+            firstName: 'Test'
+          }
+        ]
+      ]
+    });
+  });
+
+  it('should remove null properties from array of array of objects', () => {
+    const oldObject: {
+      people: {
+        firstName: string;
+        lastName: string;
+      }[][];
+    } = {
+      people: [
+        [
+          {
+            firstName: 'Test',
+            lastName: null
+          }
+        ]
+      ]
+    };
+
+    const newObject = removeEmpty(oldObject);
+
+    assert.deepStrictEqual(oldObject, {
+      people: [
+        [
+          {
+            firstName: 'Test',
+            lastName: null
+          }
+        ]
+      ]
+    });
+
+    assert.deepStrictEqual(newObject, {
+      people: [
+        [
+          {
+            firstName: 'Test'
+          }
+        ]
+      ]
+    });
+  });
+});
--- src/utils/removeEmpty.ts
+++ src/utils/removeEmpty.ts
@@ -1,10 +1,32 @@
+const removeEmptyFromArray = (array: any[]) => {
+  const newArray: any[] = array
+    .filter(v => v !== undefined && v !== null)
+    .map(v => {
+      if (Array.isArray(v)) {
+        return removeEmptyFromArray(v);
+      }
+      if (typeof v === 'object') {
+        return removeEmpty(v);
+      }
+      return v;
+    });
+  return newArray;
+};
+
 export const removeEmpty = (object: any) => {
+  const newObject: any = {};
   Object.entries(object).forEach(([key, value]) => {
     if (value === undefined || value === null) {
-      delete object[key];
-    } else if (typeof value === 'object' && !Array.isArray(value)) {
-      object[key] = removeEmpty(value);
+      return;
+    }
+
+    if (Array.isArray(value)) {
+      newObject[key] = removeEmptyFromArray(value);
+    } else if (typeof value === 'object') {
+      newObject[key] = removeEmpty(value);
+    } else {
+      newObject[key] = value;
     }
   });
-  return object;
+  return newObject;
 };
